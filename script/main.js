b8200e0ce5e5a4d5d5bbc310947e809387c11390
NClemente <nicoclemente91@gmail.com>
2021-03-08 09:17:15 -0300

    Acomodado de html y css en proceso

--- script/main.js
+++ script/main.js
@@ -1,39 +1,39 @@
-class Conversor {
+// class Conversor {
     
-    constructor(cantidad, deMoneda, aMoneda) {
-        this.cantidad = cantidad;
-        this.deMoneda = deMoneda;
-        this.aMoneda = aMoneda;
-    }
+//     constructor(cantidad, deMoneda, aMoneda) {
+//         this.cantidad = cantidad;
+//         this.deMoneda = deMoneda;
+//         this.aMoneda = aMoneda;
+//     }
     
-    conversorDeMoneda = () => {
-        if (this.deMoneda === "Dolar" & this.aMoneda === "Peso") {
-            this.cantidadConvertida = this.cantidad*90*1.3*1.3;
-        } else {
-            alert("Hubo un error");
-        }
-        return this.cantidadConvertida;
-    }
-}
+//     conversorDeMoneda = () => {
+//         if (this.deMoneda === "Dolar" & this.aMoneda === "Peso") {
+//             this.cantidadConvertida = this.cantidad*90*1.3*1.3;
+//         } else {
+//             alert("Hubo un error");
+//         }
+//         return this.cantidadConvertida;
+//     }
+// }
 
-let historial = [];
+// let historial = [];
 
-alert("Para salir del conversor ingresar un monto de 0 (cero)");
+// alert("Para salir del conversor ingresar un monto de 0 (cero)");
 
-let verificador;
+// let verificador;
 
-do {
-    let montoEnDolares = parseInt(prompt("Ingrese la cantidad de dolares que quiere comprar:"));
-    if(montoEnDolares !==0){
-        verificador = true;
-        let primeraConversion = new Conversor(montoEnDolares, "Dolar", "Peso");
-        historial.push(primeraConversion);
-        alert(`Para comprar USD${montoEnDolares} se necesitan $${primeraConversion.conversorDeMoneda()}`);
-    } else {
-        verificador = false;
-    }
-} while (verificador);
+// do {
+//     let montoEnDolares = parseInt(prompt("Ingrese la cantidad de dolares que quiere comprar:"));
+//     if(montoEnDolares !==0){
+//         verificador = true;
+//         let primeraConversion = new Conversor(montoEnDolares, "Dolar", "Peso");
+//         historial.push(primeraConversion);
+//         alert(`Para comprar USD${montoEnDolares} se necesitan $${primeraConversion.conversorDeMoneda()}`);
+//     } else {
+//         verificador = false;
+//     }
+// } while (verificador);
 
-console.log(historial);
-let montosConvertidos = historial.map(historial => historial.cantidad);
-alert(`Montos ingresados en USD: ${montosConvertidos}`);
+// console.log(historial);
+// let montosConvertidos = historial.map(historial => historial.cantidad);
+// alert(`Montos ingresados en USD: ${montosConvertidos}`);
